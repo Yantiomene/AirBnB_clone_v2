dc014ea43c5e9141ae9324c36b15da8d1d77d120
codedvictor <victorkoladeige@gmail.com>
2023-08-22 22:17:08 +0100

    class instance updated

--- models/city.py
+++ models/city.py
@@ -2,10 +2,12 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from sqlalchemy import relationship
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable = false)
     state_id = Column(String(60), ForeignKey('states.id'), nullable = false)
+    places = relationship('Place' cascade='all, delete'
+                          backref='city'
